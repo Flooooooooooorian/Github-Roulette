320e7d033dba2499a01ce369555ec3724d299513
ole-na <82842056+ole-na@users.noreply.github.com>
2021-05-21 15:57:53 +0200

    add styles for image and back link on the ProfilePage

--- frontend/src/Pages/ProfilePage.js
+++ frontend/src/Pages/ProfilePage.js
@@ -1,8 +1,19 @@
 import { useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { NavLink, useParams } from 'react-router-dom'
+import styled from 'styled-components/macro'
+import { Avatar } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(theme => ({
+  large: {
+    width: theme.spacing(30),
+    height: theme.spacing(30),
+  },
+}))
 
 export default function ProfilePage({ user, loadUser, repos, loadRepos }) {
   const { username } = useParams()
+  const classes = useStyles()
 
   useEffect(() => {
     loadUser(username)
@@ -10,10 +21,14 @@ export default function ProfilePage({ user, loadUser, repos, loadRepos }) {
   }, [])
 
   return (
-    <div>
-      <a href="javascript:history.back()">Back</a>
-      <h1> {username} </h1>
-      <img src={user.avatar_url} alt={'Profilepic not found'} />
+    <Wrapper>
+      <NavLink to="javascript:history.back()">Back</NavLink>
+      <h1>{username}</h1>
+      <Avatar
+        alt="Profilepic not found"
+        src={user.avatar_url}
+        className={classes.large}
+      />
       <details>
         <summary>Repos</summary>
         <ul>
@@ -23,6 +38,10 @@ export default function ProfilePage({ user, loadUser, repos, loadRepos }) {
         </ul>
         <p></p>
       </details>
-    </div>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  padding: 20px;
+`
