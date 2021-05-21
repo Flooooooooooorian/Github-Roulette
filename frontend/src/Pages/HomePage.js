aa440b1cde6cad814d11d1b6a63b39e263acf792
ole-na <82842056+ole-na@users.noreply.github.com>
2021-05-21 15:39:53 +0200

    add styles for HomePage

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import { useHistory } from 'react-router-dom'
+import { Button, TextField } from '@material-ui/core'
+import styled from 'styled-components/macro'
 
 export default function HomePage() {
   const history = useHistory()
@@ -14,9 +16,22 @@ export default function HomePage() {
   }
 
   return (
-    <div>
-      <input onChange={handleInputChange} value={username} />
-      <button onClick={handleButtonClick}>Los</button>
-    </div>
+    <Wrapper>
+      <TextField
+        value={username}
+        label="Username"
+        onChange={handleInputChange}
+      />
+      <Button onClick={handleButtonClick} variant="contained" color="primary">
+        Los
+      </Button>
+    </Wrapper>
   )
 }
+const Wrapper = styled.div`
+  padding: 20px;
+
+  button {
+    margin-left: 20px;
+  }
+`
