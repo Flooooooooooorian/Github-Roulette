a14bbe4d3fd20fe05b78762c09b91f0e33344e9a
Micha <Micha.amkessler@gmail.com>
2021-05-25 11:46:55 +0200

    add timer

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,14 +1,28 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import { Button, TextField } from '@material-ui/core'
 import styled from 'styled-components/macro'
+import instance from '../service/githubAPI'
 
 export default function HomePage() {
   const history = useHistory()
   const [username, setUsername] = useState('')
 
+  const refTimer = useRef(0)
+
   const handleInputChange = event => {
+    clearTimeout(refTimer.current)
     setUsername(event.target.value)
+    refTimer.current = setTimeout(searchForGithubUsers, 3000)
+  }
+
+  const searchForGithubUsers = () => {
+    console.log(username)
+    instance
+      .get('https://api.github.com/search/users?q=' + username)
+      .then(response => response.data)
+      .then(data => console.log(data))
+      .catch(error => console.error(error.message))
   }
 
   const handleButtonClick = event => {
